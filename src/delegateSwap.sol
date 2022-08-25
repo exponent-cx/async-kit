32e41df042eda011fd534cd791572226ee33f641
Exa <exa@exponent.ai>
2022-08-25 12:15:17 +0700

    update remapping

--- src/delegateSwap.sol
+++ src/delegateSwap.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.16;
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
+import "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/ClonesUpgradeable.sol";
 
 
 contract Enum {
